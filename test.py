cdb51a7b5c44234f0c10477b5186cab8b578d611
rajubuddha <rajubuddha@gmail.com>
2017-07-14 13:15:21 +0530

    test.py
    test

---
+++ test.py
@@ -0,0 +1,90 @@
+
+
+1. What is a Variable?
+2. What is variable initialization and why is it important? 
+3. What is an Array?
+4. What is a Function?
+5. What is the modulus operator? 
+6. Which of the following operators is incorrect and why? ( =>, <=, <>, ==) 
+7. What is a Method?
+8. What is a Class?
+9. What is an Object?
+10. What is a DataStracture,give a example?
+
+
+Flow Control (answer them using c/c++/javascript/python/java)
+
+11. Find the bigger number in A,B,C,D,E and if the result bigger number is less than 100 print as it is else print the sqare/2 of the result number? 
+12. Write a program to make series whitch starts with 2 and ends with 150 and the increment of 4 should not contain 34,70,86,102 ?
+13. Write a program to print the below out put using loops?
+
+4,5,6,7
+11,12,13
+17,18
+22
+
+14. Write a program to add  index*2  for the each element of the given array,program should return an Array [14,15,16,17,18,19,20]?
+15. What is break and continue statements? 
+16. What is the equivalent code of the following statement in WHILE LOOP format? 
+
+for(int i=0;i<20;i++)
+{
+ 	print (i*2);
+	print(“\n”);
+
+}
+
+17. What is a nested loop? 
+
+HTML and CSS
+
+18. Make a HTML page with a Table should contain columns like Sno,Shots,Chars,Props,Sets?
+19. Use CSS style for the above table for nice looking format?
+20. Make a simple HTML login page using text fields and a button?
+
+JavaScript
+
+21. Make a HTML page with button and button click to open google.com?
+22. What is DOM,why do we use?
+23. How create hello World text in HTML page using JavaScript?
+24. What is a “Switch”,When is a “switch” statement preferable over an “if” statement? 
+
+Python
+
+25. What is the difference between list and tuple? 
+26. What is Dict and List comprehensions are? 
+27. In Python what is slicing? 
+28. What are generators in Python? 
+29. What is the difference between Xrange and range ?
+30. Write a program for making a log file? 
+31. Why readline method? 
+32. What are the concepts of OOPs and expain them?
+33. What is function overriding and overloading?
+34. Make classes A, B  with the constructor and two proparties,methods and A should be the super calss of B?
+35. What is threading? 
+36. Write an example threading threading program for making 10 files with file name text in every file? 
+37. Read a text file in pyhton and add ; to every line's ending?
+38. How compile a string pattern in regular expression? 
+39. How search and replace using regular expression?
+40. How to run an external application from a python program?
+41. How to copy a file to a destination path? 
+
+MEL/ Maya Python
+
+42. What is a Maya Node?
+43. How to select an object using MEL/Python?
+44. Set the position of an Object using MEL/Python?
+45. Create an attribute on default prespective camera?
+46. Create  file txture using MEL/Python?
+47. Set an Attribute of a sphere_a's radius using other sphere_b's radius?
+48. Slecte all cameras using MEL/Python?
+49. Make connection from a locator Translate Z to default perspective camera's far clip?
+50. Delete all polygon objects using MEL/Python? 
+
+
+
+
+
+
+
+
